bd18711618790ea8bafad5a34b9206695b7e9a7f
Kishore K. S <kishy.sentinel@gmail.com>
2025-04-28 20:05:12 +0530

    103_binary_tree_zigzag_level_order_traversal.java

--- trees/103_binary_tree_zigzag_level_order_traversal.java
+++ trees/103_binary_tree_zigzag_level_order_traversal.java
@@ -1,3 +1,18 @@
+/*
+    For this question, Breadth First Search or BFS is the best approach to traverse a binary tree level order wise.
+        Time Complexity : O(n)
+        Space Complexity : O(n)
+    
+    Explanation:
+        We are gonna use Breadth First Search Algorithm but with a twist. we are gonna use a Deque or list to store the values of a level 
+        and we are gonna store a level in a normal manner while storing the next and previous level is stored in reverse. this type of storing 
+        gives a sense of zig-zag traversal as expected in the question! The reason to use a Deque instead of list in the code below is to avoid 
+        the O(N^2) complexity by the usage of Deque which would take O(1) time to store an element in front!
+
+*/
+
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
