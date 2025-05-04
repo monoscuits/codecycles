aa1716512c2227b48f930c321e288aa0c32db178
Kishore K. S <kishy.sentinel@gmail.com>
2025-05-04 18:47:05 +0530

    Update 114_flatten_binary_tree_to_linked_list.java

--- trees/114_flatten_binary_tree_to_linked_list.java
+++ trees/114_flatten_binary_tree_to_linked_list.java
@@ -1,3 +1,19 @@
+/*
+    Approach:
+        Depth First Search is one of the best approach to solve this problem!
+
+    Complexities:
+        Time :  O(n)
+        Space : O(n)
+
+    Explanation:
+        For every current node, we check if it has a left node then we traverse to find node with the largest element in that subtree 
+        (also known as the rightmost leaf node) then we connect the right of the current node to the rightmost leaf node. Then we make
+        current.left the new current.right and make current.left null. Then we move the current node to its right. we do this series of 
+        operation while current node is not null.
+*/
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
