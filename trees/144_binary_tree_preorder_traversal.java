cb6893f09e21b3ae465f9e0958a88c44be6906e0
Kishore K. S <kishy.sentinel@gmail.com>
2025-05-06 11:29:58 +0530

    Add files via upload

---
+++ trees/144_binary_tree_preorder_traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            
+            TreeNode current = stack.pop();
+            result.add(current.val);
+
+            if(current.right != null) stack.push(current.right);
+            if(current.left != null) stack.push(current.left);
+            
+        }
+        return result;
+    }
+    
+}
